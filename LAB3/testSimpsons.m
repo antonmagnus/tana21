b771443ce5de32b228fada2cd7513e3e1fbb2453
Adam Halim <adaab301@student.liu.se>
2021-10-26 16:18:29 +0200

    Solved 4.2

--- LAB3/testSimpsons.m
+++ LAB3/testSimpsons.m
@@ -1,13 +1,26 @@
-f = @(x) 8*x^3 - 4*x^2 + 6*x - 3;
-
-a = -4;
+f = @(x) 8*x.^3 - 4*x.^2 + 6*x - 3;
+g = @(x) sqrt(x);
+a = 1;
 b = 2;
-n = 2;
-[S, x] = SimpsonsRule(f, a, b, n);
+n = 4;
 
-syms x;
-g = 8*x^3 - 4*x^2 + 6*x - 3;
 
-I = int(g, a, b);
 
-abs(I - S)
+
+loop = [2,4,8,16];
+prev_err = 1;
+for i = 1:length(loop)
+    [S, x] = SimpsonsRule(g, a, b, loop(i));
+    I_True = integral(g,a,b);
+    err = abs(I_True - S)
+    ratio = prev_err / err
+    prev_err = err;
+end
+
+
+%syms x;
+%g = 8*x^3 - 4*x^2 + 6*x - 3;
+
+%I = int(g, a, b);
+
+
