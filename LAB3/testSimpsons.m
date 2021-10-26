e5fba1a4bd8eeb241ddd8e2509925eef2702dded
Adam Halim <adaab301@student.liu.se>
2021-10-26 15:22:24 +0200

    Solved 4.1

--- LAB3/testSimpsons.m
+++ LAB3/testSimpsons.m
@@ -1,2 +1,13 @@
 f = @(x) 8*x^3 - 4*x^2 + 6*x - 3;
-[S, x] = SimpsonsRule(f, -4, 2, 2);
+
+a = -4;
+b = 2;
+n  = 2;
+[S, x] = SimpsonsRule(f, a, b, n);
+
+syms x;
+g = 8*x^3 - 4*x^2 + 6*x - 3;
+
+I = int(g, a, b);
+
+abs(I - S)
