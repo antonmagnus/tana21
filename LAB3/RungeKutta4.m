b40dcb32f3cd8eb670b4b6c1a1c682a49d34620f
Adam Halim <adaab301@student.liu.se>
2021-10-27 15:50:27 +0200

    Solved RungeKutta

--- LAB3/RungeKutta4.m
+++ LAB3/RungeKutta4.m
@@ -21,21 +21,23 @@ function [t,y] = RungeKutta4(f,t0,T,y0,h)
 	%
 	%%
 	%  discretize the time variable and set the problem size
-	   t = t0:h:T;
-	      y = zeros(length(t),1);
-	      %%
-	      %  save the intial values
-	         y(1) = y0;
-		    t(1) = t0;
-		    %%
-		    %  use Runge-Kutta 4 stage with the given timestep h to solve the problem and
-		    %  save the information for later analysis
-		       for j = 2:length(t)
-
-
-
-
-
-			          end
+    t = t0:h:T;
+    y = zeros(length(t),1);
+    %%
+    %  save the intial values
+    y(1) = y0;
+    t(1) = t0;
+    %%
+    %  use Runge-Kutta 4 stage with the given timestep h to solve the problem and
+    %  save the information for later analysis
+    y_old = y0;
+    for j = 2:length(t)
+        k1 = f(t(j-1),y(j-1));
+        k2 = f(t(j-1) + h/2, y(j-1) + (h/2) * k1);
+        k3 = f(t(j-1)+ h/2, y(j-1) + (h/2) * k2);
+        k4 = (f(t(j-1) + h, y(j-1) + h*k3));
+        
+        y(j) = y(j-1) + ((h/6) * (k1 + 2*k2 + 2*k3 + k4));
+    end
 end
 
